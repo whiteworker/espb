1c10f970c784435fa5b2ca33c286fb74c11fb05e
zlbai <zlbai@trip.com>
2021-01-11 20:10:19 +0800

    fix

---
+++ src/pb/gen.bat
@@ -0,0 +1,1 @@
+protoc --proto_path=D:\Users\zlbai\go/src --proto_path=D:\code\Test\ginadmin\pkg\third_party --proto_path=D:\code\Test\ginadmin\internal\app\foroutapi\pb --go_out=plugins=grpc:. *.proto
