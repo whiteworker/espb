4ae1c68de58c972045e8bd1eec1b20fa4ddda0e2
zlbai <zlbai@trip.com>
2021-01-18 14:07:33 +0800

    fix normal

--- src/pb/gen.bat
+++ src/pb/gen.bat
@@ -1,3 +1,4 @@
-protoc --proto_path=D:\Users\zlbai\go/src --proto_path=D:\code\Test\ginadmin\pkg\third_party --proto_path=D:\code\Test\ginadmin\internal\app\foroutapi\pb --go_out=plugins=grpc:. *.proto
+for  /r  %%i  in  (*.proto)  do  (echo  generating  %%i  &  kratos tool protoc protoc --proto_path=D:\Users\zlbai\go/src --proto_path=D:\code\Test\ginadmin\pkg\third_party --proto_path=D:\code\Test\ginadmin\internal\app\foroutapi\pb --go_out=plugins=grpc:. %%i )
 xcopy "*" "../*" /S/F/I/R/Y
-del *.json &&del *.go && cd ../&& del *.bat&& del *.proto &&cd pb
+git checkout . && git clean -xdf 
+cd ../
