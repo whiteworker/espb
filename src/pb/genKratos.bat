2c49ec59a429b47b4908595443b1890640d3ecd0
zlbai <zlbai@trip.com>
2021-01-18 11:38:29 +0800

    fix

--- src/pb/genKratos.bat
+++ src/pb/genKratos.bat
@@ -1,8 +1,11 @@
-@echo off
-set command = " swagger mixin "
+
 for  /r  %%i  in  (*.proto)  do  (echo  generating  %%i  &  kratos tool protoc %%i )
 xcopy "*" "../*" /S/F/I/R/Y
 git checkout . && git clean -xdf 
 cd ../
-for  /r  %%i  in  (*.json)  do  (  set command=%command%,%%i)
-del *.bat&& echo %commmand%
+set command=swagger mixin 
+for  /r  %%i  in  (*.json)  do call set command=%%command%% %%i
+set command=%command% -o swagger.json
+%command%
+del *.bat
+cd pb
