692871e273f6a9f68bbb0c30d56a7f7f4c35eb20
zlbai <zlbai@trip.com>
2021-01-21 16:19:02 +0800

    remove bat move into makefile

--- src/pb/gen.bat
+++
@@ -1,4 +0,0 @@
-for  /r  %%i  in  (*.proto)  do  (echo  generating  %%i  &  protoc --proto_path=D:\Users\zlbai\go/src --proto_path=D:\code\Test\ginadmin\pkg\third_party --proto_path=D:\code\Test\ginadmin\internal\app\foroutapi\pb --gofast_out=plugins=grpc:. %%i )
-xcopy "*" "../*" /S/F/I/R/Y
-git checkout . && git clean -xdf 
-cd ../
