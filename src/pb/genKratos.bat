c675666934d5c86fa2513071710c57b1ecb491c9
zlbai <zlbai@trip.com>
2021-01-21 16:19:34 +0800

    Revert "remove bat move into makefile"
    This reverts commit 692871e273f6a9f68bbb0c30d56a7f7f4c35eb20.

---
+++ src/pb/genKratos.bat
@@ -0,0 +1,11 @@
+
+for  /r  %%i  in  (*.proto)  do  (echo  generating  %%i  &  kratos tool protoc %%i )
+xcopy "*" "../*" /S/F/I/R/Y
+git checkout . && git clean -xdf 
+cd ../
+del *.bat && del *.json
+set command=swagger mixin 
+for  /r  %%i  in  (*.json)  do call set command=%%command%% %%i
+set command=%command% -o "../api/swagger/swagger.json"
+%command%
+cd pb
